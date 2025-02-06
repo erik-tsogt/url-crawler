7fcf288f653837bf4b17a8616756d44fe255be11
Erkhembayar (Erik) Tsogtbaatar <148504714+erik-tsogt@users.noreply.github.com>
2025-02-05 20:24:41 -0800

    Implemented a simple fetchPage function to fetch a page

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,1 +1,18 @@
-console.log('Crawler is running');
+const axios = require("axios");
+//const cheerio = require("cheerio");
+const targetUrl = "https://stackoverflow.com/questions";
+
+const fetchPage = async (url) => {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (err) {
+        console.error(`Error fetching ${url}`, err.message);
+        return null;
+    }
+}
+
+fetchPage(targetUrl).then((html) => {
+    if (html) console.log(html.substring(0, 500));
+})
+
